dbbc0ae0cd7fe0eb78d3fcee053ebe95521d70bf
Laura <lauraalspaugh2002@icloud.com>
2024-04-04 16:56:19 -0600

    new react practice

---
+++ React-app/demo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>
+        Hello World!
+      </p>
+      <img src='https://images.unsplash.com/photo-1711861413115-797ee0655214?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8'></img>
+      
+    </div>
+  );
+}
+
+export default App;
