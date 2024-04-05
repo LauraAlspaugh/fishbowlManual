519313c7c452064b723d93dd50cd8a664e1f1ee7
Laura <lauraalspaugh2002@icloud.com>
2024-04-05 10:30:15 -0600

    how do columns work

--- React-app/demo/src/App.js
+++ React-app/demo/src/App.js
@@ -3,15 +3,9 @@ import './App.css';
 import Profile from './Profile.tsx';
 
 function App() {
+
   return (
     <div>
-      <p>
-        Hello World!
-      </p>
-      <img 
-      className='martini'
-      src='https://images.unsplash.com/photo-1711861413115-797ee0655214?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8'></img>
-      
       <Profile/>
     </div>
   );
--- React-app/demo/src/Profile.tsx
+++ React-app/demo/src/Profile.tsx
@@ -1,21 +1,26 @@
 import React from "react"
 
 export default function Profile() {
+    const message = 'hello World! Welcome to React Learning.'
     return (
-        <div>
+        <div
+        style={{
+            columnCount: '12',
+            justifyContent: 'center',
+           display: 'flex'
+        }}
+        >
+      <p style={{
+       color: 'pink',
+       fontFamily: 'fantasy',
+       fontSize: '30px',
+       padding: '5px'
+      }}>
+        {message}
+      </p>
       <img 
-      className="profile"
-        src="https://i.imgur.com/MK3eW3Am.jpg"
-        alt="Katherine Johnson"
-      />
-      <img 
-        src="https://i.imgur.com/MK3eW3Am.jpg"
-        alt="Katherine Johnson"
-      />
-      <img 
-        src="https://i.imgur.com/MK3eW3Am.jpg"
-        alt="Katherine Johnson"
-      />
+      className='martini'
+      src='https://images.unsplash.com/photo-1711861413115-797ee0655214?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8'></img>
       </div>
     )
   }
