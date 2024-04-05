1130986fc489d94f6aea6ec42b04b18ef13bf28d
Laura <lauraalspaugh2002@icloud.com>
2024-04-05 10:15:15 -0600

    working with classnames

--- React-app/demo/src/App.js
+++ React-app/demo/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Profile from './Profile.tsx';
 
 function App() {
   return (
@@ -7,8 +8,11 @@ function App() {
       <p>
         Hello World!
       </p>
-      <img src='https://images.unsplash.com/photo-1711861413115-797ee0655214?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8'></img>
+      <img 
+      className='martini'
+      src='https://images.unsplash.com/photo-1711861413115-797ee0655214?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8'></img>
       
+      <Profile/>
     </div>
   );
 }
---
+++ React-app/demo/src/Profile.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Profile() {
+    return (
+        <div>
+      <img 
+      className="profile"
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+      <img 
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+      <img 
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+      </div>
+    )
+  }
