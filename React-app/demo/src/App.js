e607cc03a930c97b023b296c21c7f9644455bca4
Laura <lauraalspaugh2002@icloud.com>
2024-04-09 15:37:16 -0600

    importing bootstrap

--- React-app/demo/src/App.js
+++ React-app/demo/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Profile from './Profile.tsx';
+import { Col} from "react-bootstrap/Col";
 
 function App() {
 
   return (
     <div>
+      <Col  sm={6} md={12}>
       <Profile/>
+
+      </Col>
     </div>
   );
 }
--- React-app/demo/src/Profile.tsx
+++ React-app/demo/src/Profile.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import { Col} from "react-bootstrap/Col";
 
 export default function Profile() {
     const message = 'Hello World! Welcome to React Learning.'
@@ -10,7 +11,10 @@ export default function Profile() {
            display: 'flex'
         }}
         >
+            <Col sm={6} md={12}> 
+
       <p style={{
+        columnRuleWidth: 12,
        color: 'pink',
        fontFamily: 'fantasy',
        fontSize: '30px',
@@ -18,6 +22,7 @@ export default function Profile() {
       }}>
         {message}
       </p>
+            </Col>
       <img 
       className='martini'
       src='https://images.unsplash.com/photo-1711861413115-797ee0655214?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8'></img>
