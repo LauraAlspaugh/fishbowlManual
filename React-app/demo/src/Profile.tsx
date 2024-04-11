da66b65a70e6bd2a90feb706023dbaa44fd0c0e3
Laura <lauraalspaugh2002@icloud.com>
2024-04-11 16:54:43 -0600

    ordered lists vs unordered lists

--- React-app/demo/src/Profile.tsx
+++ React-app/demo/src/Profile.tsx
@@ -3,13 +3,21 @@ import React from "react";
 
 
 export default function Profile() {
-    const message = 'Hello World! Welcome to React Learning.'
+    
     return (
       <div>  
-      <p className="profile">
-        {message}
-      </p>
-      <img 
+      
+        <h1 className="profile">Fun Facts About React</h1>
+        <ul>
+          <li>Was First Released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on Github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+       
+      
+      <img alt="martini"
       className='martini'
       src='https://images.unsplash.com/photo-1711861413115-797ee0655214?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8'></img>
             
