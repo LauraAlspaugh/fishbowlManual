2edcdbdbc58faf3560966d326115558b25af8ac3
Laura <lauraalspaugh2002@icloud.com>
2024-04-12 14:36:26 -0600

    first project

--- React-app/demo/src/App.js
+++ React-app/demo/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
 import Profile from './Profile.tsx';
-import Picture from './Picture.tsx';
+
 
 
 
 function App() {
 
   return (
-    <div>
+    <div className='page'>
+      <nav className='navbar'>
+        <img className='image' alt='react' src="../logo192.png"></img>
+        <h1 className='react-facts'>ReactFacts</h1>
+        <h2 className='project'>React Course - Project 1</h2>
+      </nav>
      
       <Profile/>
-<Picture/>
+
      
     </div>
   );
--- React-app/demo/src/Profile.tsx
+++ React-app/demo/src/Profile.tsx
@@ -5,10 +5,10 @@ import React from "react";
 export default function Profile() {
     
     return (
-      <div>  
+      <main>  
       
         <h1 className="profile">Fun Facts About React</h1>
-        <ul>
+        <ul className="list">
           <li>Was First Released in 2013</li>
           <li>Was originally created by Jordan Walke</li>
           <li>Has well over 100K stars on Github</li>
@@ -17,11 +17,9 @@ export default function Profile() {
           </ul>
        
       
-      <img alt="martini"
-      className='martini'
-      src='https://images.unsplash.com/photo-1711861413115-797ee0655214?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8'></img>
+      
             
-      </div>
+      </main>
      
     )
   }
